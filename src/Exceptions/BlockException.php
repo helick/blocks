c19a7d20f0897c8098010a4b42eef7a9399f581b
Nasyrov Evgenii <inasyrov@ya.ru>
2019-06-19 21:53:56 +0100

    Add additional block exception declarations

--- src/Exceptions/BlockException.php
+++ src/Exceptions/BlockException.php
@@ -13,4 +13,24 @@ final class BlockException extends InvalidArgumentException
     {
         return new static('You must set the block name before composing');
     }
+
+    /**
+     * @return BlockException
+     */
+    public static function forEmptyTemplate(): self
+    {
+        return new static('You must set the block template before rendering');
+    }
+
+    /**
+     * @param mixed $template
+     *
+     * @return BlockException
+     */
+    public static function forNotFoundTemplate($template): self
+    {
+        return new static(strtr('The block template ":template" could not be found', [
+            ':template' => implode(', ', (array)$template)
+        ]));
+    }
 }

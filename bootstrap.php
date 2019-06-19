ca02ab29f08fb540f6349ff24f5f6d8cd3565041
Nasyrov Evgenii <inasyrov@ya.ru>
2019-06-19 19:08:45 +0100

    Add plugin bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:    Helick Blocks
+Author:         Evgenii Nasyrov
+Author URI:     https://helick.io/
+*/
+
+// Require Composer autoloader if installed on it's own
+if (file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
+    require_once $composer;
+}
+
+// Helpers
+
+// Services

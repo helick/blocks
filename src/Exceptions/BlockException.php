a9511ec2259171a7e49656892d73e8f58777b055
Nasyrov Evgenii <inasyrov@ya.ru>
2019-06-19 21:38:01 +0100

    Rename block exception

--- src/Exceptions/BlockException.php
+++ src/Exceptions/BlockException.php
@@ -4,12 +4,12 @@ namespace Helick\Blocks\Exceptions;
 
 use InvalidArgumentException;
 
-final class BlockNameEmptyException extends InvalidArgumentException
+final class BlockException extends InvalidArgumentException
 {
     /**
-     * @return BlockNameEmptyException
+     * @return BlockException
      */
-    public static function withMessage(): self
+    public static function forEmptyName(): self
     {
         return new static('You must set the block name before composing');
     }

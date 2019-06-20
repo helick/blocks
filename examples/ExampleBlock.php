e514ecf1c85e1c987b6ab7db47cd828c86fed3a2
Nasyrov Evgenii <inasyrov@ya.ru>
2019-06-20 15:36:14 +0100

    Update example block

--- examples/ExampleBlock.php
+++ examples/ExampleBlock.php
@@ -2,6 +2,7 @@
 
 use Carbon_Fields\Field;
 use Helick\Blocks\Block;
+use WP_Query;
 
 final class ExampleBlock extends Block
 {
@@ -37,6 +38,46 @@ final class ExampleBlock extends Block
             Field::make('text', 'heading', 'Heading'),
             Field::make('image', 'image', 'Image'),
             Field::make('rich_text', 'content', 'Content'),
+            Field::make('association', 'associations', 'Associations')
+                 ->set_types([
+                     [
+                         'type'      => 'post',
+                         'post_type' => 'post',
+                     ]
+                 ])
         ];
+    }
+
+    /**
+     * Data to be passed to the rendered block.
+     *
+     * @param array $fields
+     *
+     * @return array
+     */
+    public function with(array $fields): array
+    {
+        return [
+            'associations' => $this->queryAssociations($fields['associations'])
+        ];
+    }
+
+    /**
+     * Query the associations.
+     *
+     * @param array $associations
+     *
+     * @return WP_Query
+     */
+    private function queryAssociations(array $associations): WP_Query
+    {
+        $associationIds = array_column($associations, 'id');
+        $associationIds = array_map('intval', $associationIds);
+
+        return new WP_Query([
+            'no_found_rows' => true,
+            'post__in'      => $associationIds,
+            'orderby'       => 'post__in',
+        ]);
     }
 }

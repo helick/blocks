1dc8c3a6963433cca6855c117f4e75f6b8733c41
Evgenii Nasyrov <inasyrov@ya.ru>
2019-06-20 19:16:26 +0100

    Traits clean up (#4)
    * Move common block declaration back in main class
    
    * Reorder composable declarations
    
    * Reorder composable declarations

--- src/Block.php
+++ src/Block.php
@@ -4,11 +4,59 @@ namespace Helick\Blocks;
 
 abstract class Block implements Contracts\Bootable, Contracts\Composable
 {
-    use Traits\CommonDeclaration,
-        Traits\NestedDeclaration,
+    use Traits\NestedDeclaration,
         Traits\Bootable,
         Traits\Composable,
         Traits\Renderable;
+
+    /**
+     * The block's display name.
+     *
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * The block's description.
+     *
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * The block's icon.
+     *
+     * @var string
+     */
+    protected $icon = '';
+
+    /**
+     * The block's category.
+     *
+     * @var string
+     */
+    protected $category = '';
+
+    /**
+     * The block's keywords.
+     *
+     * @var string[]
+     */
+    protected $keywords = [];
+
+    /**
+     * The block's preview mode.
+     *
+     * @var bool
+     */
+    protected $preview = true;
+
+    /**
+     * The block's template(s).
+     *
+     * @var string|string[]
+     */
+    protected $template = '';
 
     /**
      * Fields to be attached to the block.
--- src/Traits/Composable.php
+++ src/Traits/Composable.php
@@ -21,15 +21,16 @@ trait Composable
         CarbonFieldsBlock::make($this->name)
                          ->add_fields($this->fields())
                          ->set_description($this->description)
-                         ->set_category($this->category)
                          ->set_icon($this->icon)
+                         ->set_category($this->category)
                          ->set_keywords($this->keywords)
+                         ->set_preview_mode($this->preview)
+                         ->set_parent($this->parent)
                          ->set_inner_blocks($this->nested)
                          ->set_inner_blocks_position($this->nestedPosition)
                          ->set_inner_blocks_template($this->nestedTemplates)
                          ->set_inner_blocks_template_lock($this->nestedLock)
                          ->set_allowed_inner_blocks($this->nestedBlocks)
-                         ->set_parent($this->parent)
                          ->set_render_callback([$this, 'render']);
     }
 }
